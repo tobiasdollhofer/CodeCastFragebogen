ff0057aa8556703bebdadcf297ffdb4f8993902f
tobiasdollhofer <tdollhofer96@googlemail.com>
2021-07-07 16:34:09 +0200

    fixed compatibility issue of webbrowser

--- src/main/java/Viewer.java
+++ src/main/java/Viewer.java
@@ -1,12 +1,18 @@
 
+import com.intellij.ui.jcef.JBCefApp;
 import com.intellij.ui.jcef.JBCefBrowser;
 import log.CsvLogger;
 
 import javax.swing.*;
+import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Viewer {
 
-    private static String BROWSER_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfW-vDJk4x4B325IhCI9nCzH3h8hSUArPkI5bzDJZsUtHHsGw/viewform?usp=sf_link";
+    private static String BROWSER_URL = "https://forms.gle/P8jMtnnQkfJSwcxM8";
 
     private JPanel windowContent;
     private JTextField headline;
@@ -16,17 +22,52 @@ public class Viewer {
     private JBCefBrowser browser;
 
     public Viewer() {
-        browser = new JBCefBrowser(BROWSER_URL);
-        submitButton.addActionListener((e) -> {
-            windowContent.remove(headline);
-            windowContent.remove(description);
-            windowContent.remove(submitButton);
-            windowContent.remove(importantField);
-            windowContent.setLayout(new BoxLayout(windowContent, BoxLayout.Y_AXIS));
+        if(JBCefApp.isSupported()){
+            browser = new JBCefBrowser(BROWSER_URL);
+            submitButton.addActionListener((e) -> {
+                windowContent.remove(headline);
+                windowContent.remove(description);
+                windowContent.remove(submitButton);
+                windowContent.remove(importantField);
+                windowContent.setLayout(new BoxLayout(windowContent, BoxLayout.Y_AXIS));
 
-            windowContent.add((JPanel) browser.getComponent());
-            CsvLogger.sendToServer();
-        });
+                windowContent.add((JPanel) browser.getComponent());
+                CsvLogger.sendToServer();
+            });
+        }else{
+            submitButton.addActionListener((e) -> {
+                try{
+                    openWebpage(new URL(BROWSER_URL));
+                    CsvLogger.sendToServer();
+                }catch (MalformedURLException ex){
+                    ex.printStackTrace();
+                }
+
+            });
+        }
+
+    }
+
+    public static boolean openWebpage(URI uri) {
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            try {
+                desktop.browse(uri);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean openWebpage(URL url) {
+        try {
+            return openWebpage(url.toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public JPanel getContent(){
