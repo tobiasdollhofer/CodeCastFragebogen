5583e1812c93382eccc780e3760b748e11c5d847
tobiasdollhofer <tdollhofer96@googlemail.com>
2021-07-09 10:54:12 +0200

    implemented logging of caret line content

--- src/main/java/log/listeners/MyCaretListener.java
+++ src/main/java/log/listeners/MyCaretListener.java
@@ -2,6 +2,7 @@ package log.listeners;
 
 import com.intellij.openapi.editor.event.CaretEvent;
 import com.intellij.openapi.editor.event.CaretListener;
+import com.intellij.openapi.util.TextRange;
 import log.Context;
 import log.CsvLogger;
 import log.EventType;
@@ -14,8 +15,20 @@ public class MyCaretListener implements CaretListener {
         CaretListener.super.caretPositionChanged(event);
         int line = event.getEditor().getCaretModel().getLogicalPosition().line + 1;
         int col = event.getEditor().getCaretModel().getLogicalPosition().column;
-        CsvLogger.log(Context.EDITOR, EventType.CARET_POSITION_CHANGED, line + ":" + col);
+
+        String text = getLineContent(event, line-1);
+        CsvLogger.log(Context.EDITOR, EventType.CARET_POSITION_CHANGED, line + ":" + col + ", line content: " + text);
     }
 
-
+    /**
+     *
+     * @param e caret event
+     * @param line line of caret
+     * @return text of code line
+     */
+    private String getLineContent(CaretEvent e, int line){
+        int offsetStart = e.getEditor().getDocument().getLineStartOffset(line);
+        int offsetEnd = e.getEditor().getDocument().getLineEndOffset(line);
+        return e.getEditor().getDocument().getText(new TextRange(offsetStart, offsetEnd)).trim();
+    }
 }
